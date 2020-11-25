f4626dd6f7632aee84de2549f79db5d87d05bdba
Jon Schwamman <Jon.Schwamman@ccr.net>
2020-11-25 14:40:22 -0600

    Added header and letterboard components

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="header">
+    <span>Letter Board</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'LetterBoard'
+  }
+</script>
+
+<style lang="scss" scoped>
+  #header {
+    background-color: #493843;
+    color: #FFFDED;
+    height: 60px;
+
+    span {
+      display: block;
+      font-size: 32px;
+      padding-top: 10px;
+    }
+  }
+</style>
---
+++ src/components/LetterBoard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'LetterBoard',
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
